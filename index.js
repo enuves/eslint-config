64b9253c8af4bee4da0a859270792bbaecaf2519
Welkson Ramos <welksonr@hotmail.com>
2021-03-15 09:00:46 -0300

    chore: add initial rules

---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = {
+  rules: {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "eqeqeq": [
+      "error",
+      "always"
+    ], // === and !==
+    "init-declarations": [
+      "error",
+      "always"
+    ],
+    "no-var": "error",
+    "key-spacing": [
+      "error",
+      { "afterColon": true }
+    ],
+    "no-empty-function": "error",
+    "space-before-function-paren": [
+      "error",
+      "always"
+    ],
+    "func-call-spacing": [
+      "error",
+      "always",
+      { "allowNewlines": true }
+    ],
+    "arrow-body-style": [
+      "error",
+      "as-needed"
+    ], //es6
+    "arrow-parens": [
+      "error",
+      "as-needed"
+    ],
+    "arrow-spacing": [
+      "error", {
+        "before": true,
+        "after": true
+      }
+    ]
+  }
+};
